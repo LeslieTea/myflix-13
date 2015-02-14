7c9946a1ff6c1b5e129a8bf009e5d6105aa143c3
Leslietea <a.leslieann@gmail.com>
2015-02-14 01:09:45 +0000

    Video index, show. Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Myflix::Application.routes.draw do
+  root to: 'videos#index'
+  
+  
+  get 'home', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
+  get '/videos/:id', to: 'videos#show', as: 'video'
+  
+  resources :videos, only: [:show] do
+  end
+  
 end
