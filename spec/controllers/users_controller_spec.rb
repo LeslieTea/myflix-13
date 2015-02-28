e75f6b14ff18d9f7ef972e017110f6da6e96ec99
Leslietea <a.leslieann@gmail.com>
2015-02-28 04:03:18 +0000

    User controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,4 +7,40 @@ describe UsersController do
       expect(assigns(:user)).to be_instance_of(User)
     end
   end
+
+  
+  describe "POST create" do
+    context "with valid input" do
+           
+      it "creates the user" do
+        post :create, user: { email: "leslie@example.com", password: "password", full_name: "Leslie Turner" }
+        expect(User.count).to eq(1)
+      end
+      
+      it "redirects to sign in page" do
+        post :create, user: { email: "leslie@example.com", password: "password", full_name: "Leslie Turner" }
+        expect(response).to redirect_to sign_in_path
+      end
+    end
+    
+    context "with invalid input" do
+         
+      it "doesn't create the user" do
+        post :create, user: { password: "password", full_name: "Leslie Turner" }
+        expect(User.count).to eq(0)
+      end
+      
+      it "renders the :new template" do
+        post :create, user: { password: "password", full_name: "Leslie Turner" }
+        expect(response).to render_template :new
+      end
+      
+      it "sets @user" do
+        post :create, user: { password: "password", full_name: "Leslie Turner" }
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
+
+
