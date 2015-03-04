db5f0e9fd5e3b170d22ca19372f97fd3fd24285f
Leslietea <a.leslieann@gmail.com>
2015-03-04 03:51:24 +0000

    Review test for un/authenticated users

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -12,12 +12,12 @@ describe ReviewsController do
           video = Fabricate(:video)
           post :create, review: Fabricate.attributes_for(:review), video_id: video.id
           expect(response).to redirect_to video
-        end
+         end
          it "creates a review" do
            video = Fabricate(:video)
            post :create, review: Fabricate.attributes_for(:review), video_id: video.id
            expect(Review.count).to eq(1)
-         end
+        end
         it "creates a review associated with the video" do
            video = Fabricate(:video)
            post :create, review: Fabricate.attributes_for(:review), video_id: video.id
@@ -29,8 +29,37 @@ describe ReviewsController do
           expect(Review.first.user).to eq(current_user)
         end
       end
-      context "with invalid inputs"
+      context "with invalid inputs" 
+        it "does not create a review" do
+          video = Fabricate(:video)
+          post :create, review: {rating: 4}, video_id: video.id
+          expect(Review.count).to eq(0)
+        end
+        it "renders the videos/show template" do
+          video = Fabricate(:video)
+          post :create, review: {rating: 4}, video_id: video.id
+          expect(response). to render_template "videos/show"
+        end
+      it "sets @video" do
+        video = Fabricate(:video)
+        post :create, review: {rating: 4}, video_id: video.id
+        expect(assigns(:video)).to eq(video)
+      end
+      it "sets @reviews" do
+        video = Fabricate(:video)
+        review = Fabricate(:review, video: video)
+        post :create, review: {rating: 4}, video_id: video.id
+        expect(assigns(:reviews)).to match_array([review])
+      end
     end
-    context "with unauthenticated users"
   end
+  
+  context "with unauthenticated users" do
+    it "redirects to te sign in path" do
+      video = Fabricate(:video)
+      post :create, review: Fabricate.attributes_for(:review), video_id: video.id
+      expect(response).to redirect_to sign_in_path
+    end
+  end
+  
 end
