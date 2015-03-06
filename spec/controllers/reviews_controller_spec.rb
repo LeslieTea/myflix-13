46ea76140d59835f70921530f755ca29dc21a673
Leslietea <a.leslieann@gmail.com>
2015-03-06 02:00:51 +0000

    review test complete

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -13,34 +13,41 @@ describe ReviewsController do
         before do
           post :create, review: Fabricate.attributes_for(:review), video_id: video.id
         end
+        
         it "redirects to video show page" do
           expect(response).to redirect_to video
         end
+        
         it "creates a review" do
           expect(Review.count).to eq(1)
         end
+        
         it "creates a review associated with the video" do
           expect(Review.first.video).to eq(video)
         end
+        
         it "Creates a review associated with the signed in user" do
           expect(Review.first.user).to eq(current_user)
         end
       end
   
       
-    context "with invalid inputs" 
+    context "with invalid inputs" do
       it "does not create a review" do
         post :create, review: {rating: 4}, video_id: video.id
         expect(Review.count).to eq(0)
       end
+      
       it "renders the videos/show template" do
         post :create, review: {rating: 4}, video_id: video.id
         expect(response). to render_template "videos/show"
       end
+      
       it "sets @video" do
         post :create, review: {rating: 4}, video_id: video.id
         expect(assigns(:video)).to eq(video)
       end
+      
       it "sets @reviews" do
         review = Fabricate(:review, video: video)
         post :create, review: {rating: 4}, video_id: video.id
@@ -49,12 +56,14 @@ describe ReviewsController do
     end
   end
   
+  
   context "with unauthenticated users" do
     before do
-      post :create, review: Fabricate.attributes_for(:review), video_id: @video.id
+      post :create, review: Fabricate.attributes_for(:review), video_id: video.id
     end
     it "redirects to the sign in path" do
       expect(response).to redirect_to sign_in_path
     end
   end
 end 
+end
