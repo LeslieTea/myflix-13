83373dad1e3487f8b70e1a8bf8511367f7718177
Leslietea <a.leslieann@gmail.com>
2015-03-31 02:55:21 +0000

    Follower a person

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,21 @@ class User < ActiveRecord::Base
   has_many :following_relationships, class_name: "Relationship", foreign_key: :follower_id 
   has_many :leading_relationships, class_name: "Relationship", foreign_key: :leader_id
   
-  def queued_video?(video)
-    queue_items.map(&:video).include?(video)
-  end
-  
   def normalize_queue_item_positions
     queue_items.each_with_index do |queue_item, index|
       queue_item.update(position: index + 1)
     end
+  end
+    
+  def queued_video?(video)
+    queue_items.map(&:video).include?(video)
+  end
+  
+  def follows?(another_user)
+    following_relationships.map(&:leader).include?(another_user)
+  end
+  
+  def can_follow?(another_user)
+    !(self.follows?(another_user) || self == another_user)
   end
 end
