921785dab3c5bc773196de53c0d9ba6a00df3439
Leslietea <a.leslieann@gmail.com>
2015-04-30 02:10:05 +0000

    Invitation create & test

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,4 +5,15 @@ class InvitationsController < ApplicationController
     @invitation = Invitation.new
   end
   
+def create
+  Invitation.create(invitation_params.merge(inviter_id: current_user.id))
+  redirect_to new_invitation_path
+end
+
+private
+
+def invitation_params
+  params.require(:invitation).permit(:recipient_name, :recipient_email, :message)
+end
+  
 end
--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -13,4 +13,26 @@ describe InvitationsController do
       let(:action) { get :new }
     end
   end
+  
+  describe "POST create" do
+    it_behaves_like "requires sign in" do
+      let(:action) { post :create }
+    end
+    
+    context "with valid input" do
+      it "creates an invitation" do
+        set_current_user
+        post :create, invitation: { recipient_name: "Joe Smith", recipient_email: "joe@example.com", message: "Hey join Myflix!" }
+        expect(Invitation.count).to eq(1)
+      end
+      it "sends an email to the recipient"
+      it "redirects to the invitation new page" do
+        set_current_user
+        post :create, invitation: { recipient_name: "Joe Smith", recipient_email: "joe@example.com", message: "Hey join Myflix!" }
+        expect(response).to redirect_to new_invitation_path
+      end
+      it "sets the flash success message"
+    end
+    context "with invalid input"
+  end
 end
