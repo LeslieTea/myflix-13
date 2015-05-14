9bd980ae416b2d2df5d12d8a22126911e0256270
Leslietea <a.leslieann@gmail.com>
2015-05-14 01:36:37 +0000

    tokenable shared examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-
 describe User do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
@@ -8,10 +7,9 @@ describe User do
   it { should validate_uniqueness_of(:email) }
   it { should have_many(:queue_items), -> { order (:position) }}
   it { should have_many(:reviews), -> { order ("created_at DESC") }}
-
-  it "generates a random token when the user is created" do
-    alice = Fabricate(:user)
-    expect(alice.token).to be_present
+ 
+  it_behaves_like "tokenable" do
+    let(:object) { Fabricate(:user) }
   end
   
   describe "#queued_video?" do
